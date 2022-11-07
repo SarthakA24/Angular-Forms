78453ea9ff123e3c54c6f06907804a57e1fede06
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-11-07 13:00:57 +0530

    added variables for form fields

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  userName!: string;
+  passWord!: string;
+  phone!:number;
+
   submitDetails() {
     
   }
